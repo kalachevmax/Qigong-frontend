a73ec384098294d9a7f5b8dbdfb951a920afabc4
Max Kalachev <maxkalachev@yandex.ru>
2014-10-12 01:01:32 +0400

    dynamic quotes

--- app/modules/home/controller.js
+++ app/modules/home/controller.js
@@ -1,4 +1,22 @@
 
 
-app.controller('homeController', ['$scope', function($scope) {
+app.controller('homeController', ['$scope', '$http', 'appConfig', function($scope, $http, appConfig) {
+  $scope.quote = 'ЦИТАТА';
+
+  $scope.getRandomQuote = function() {
+    $http.get(appConfig.apiUrl + 'random-quote')
+        .success(handleSuccess).error(handleError);
+
+    function handleSuccess(data, status, headers) {
+      $scope.quote = data;
+    }
+
+    function handleError() {
+    }
+  };
+
+  $scope.$on('$viewContentLoaded', function(){
+    console.log('$viewContentLoaded');
+    $scope.getRandomQuote();
+  });
 }]);
